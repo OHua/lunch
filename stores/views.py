3e29b1a89ef22d0e8c766a082cb4cccde2a29fb0
OHua <OHua@users.noreply.github.com>
2017-11-28 17:45:08 +0800

    '第十三周進度完成'

--- stores/views.py
+++ stores/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+# stores/view.py
+
+from django.shortcuts import redirect,render
+from django.forms.models import modelform_factory
 from .models import Store
+
 #from django.http import HttpResponse
 
 # Create your views here.
@@ -14,3 +18,31 @@ def store_detail(request, pk):
     except Store.DoesNotExist:
         raise Http404
     return render(request, 'stores/store_detail.html', {'store': store})
+	
+def store_create(request):
+    StoreForm = modelform_factory(Store, fields=('name', 'notes',))
+    if request.method == 'POST':
+        form = StoreForm(request.POST)
+        if form.is_valid():
+            store = form.save()
+            return redirect(store.get_absolute_url())
+    else:
+        form = StoreForm()
+    return render(request, 'stores/store_create.html', {'form': form})
+	
+def store_update(request, pk):
+    try:
+        store = Store.objects.get(pk=pk)
+    except Store.DoesNotExist:
+        raise Http404
+    StoreForm = modelform_factory(Store, fields=('name', 'notes',))
+    if request.method == 'POST':
+        form = StoreForm(request.POST, instance=store)
+        if form.is_valid():
+            store = form.save()
+            return redirect(store.get_absolute_url())
+    else:
+        form = StoreForm(instance=store)
+    return render(request, 'stores/store_update.html', {
+        'form': form, 'store': store,
+    })
